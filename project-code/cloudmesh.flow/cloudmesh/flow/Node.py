42693f77ad87bcf147bd287088f288395588a84d
Rob Ludwig <robludwig@gmail.com>
2019-03-29 10:05:45 -0400

    latest db code

--- project-code/cloudmesh.flow/cloudmesh/flow/Node.py
+++ project-code/cloudmesh.flow/cloudmesh/flow/Node.py
@@ -1,11 +1,15 @@
 class Node():
     def __init__(self, name):
         self.name = name
-        pass
+        self.dependencies = []
+        self.workflow = ""
 
     def add_dependency(self, other_node):
-        pass
+        self.dependencies.append(other_node.name)
     
+    def toDict(self):
+        return {"name": self.name, "dependencies" : self.dependencies, "workflow" : self.workflow}
+
     def __repr__(self):
        return self.name
 
--- project-code/cloudmesh.flow/cloudmesh/flow/WorkFlow.py
+++ project-code/cloudmesh.flow/cloudmesh/flow/WorkFlow.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import re
+from cloudmesh.mongo.CmDatabase import CmDatabase
 from Node import Node
 
 SPLIT_CHARS = ["\|", "&", ";"]
@@ -12,18 +13,31 @@ class WorkFlow:
         self.flowstring = flowstring
         nodes = SPLIT_RE.split(flowstring)
         flow_nodes = []
+        self.database = CmDatabase()
         self.name = name
         for node in nodes:
             flow_node = Node(node)
             flow_node.workflow = name 
             print(flow_node)
             flow_nodes.append(flow_node)
+            self.database.insert(flow_node.toDict())
+    
     def __repr__(self):
         return " ".join([self.name, self.flowstring])
+    
 
     def run(self):
-        pass
+        collection = self.database.collection("workflow")
+        initial_nodes = collection.find({"workflow" : self.name, "dependencies.0" : {"$exists" : False}})
+        for initial_node in initial_nodes:
+            self.run_node(initial_node)
+
+    def run_node(self, node):
+        print("running node", node)
 
 if __name__ == "__main__":
     flowstring = sys.argv[1]
-    print(WorkFlow("myflow", flowstring))
+    flow = WorkFlow("myflow", flowstring)
+    print(flow)
+    flow.run()
+            
