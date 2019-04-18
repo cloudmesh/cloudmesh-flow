e42919146b018fc8fce85fca28684181a4b37dfe
Rob Ludwig <rwludwig@iu.edu>
2019-04-18 16:19:08 -0400

    stub all tests

--- project-code/cloudmesh-flow/tests/test_flowdb.py
+++ project-code/cloudmesh-flow/tests/test_flowdb.py
@@ -1,7 +1,7 @@
 ###############################################################
-# pip install .; pytest -v --capture=no -v --nocapture tests/test_flownode.py:Test_flownode.test_001
-# pytest -v --capture=no tests/test_flownode.py
-# pytest -v  tests/test_flownode.py
+# pip install .; pytest -v --capture=no -v --nocapture tests/test_flowdb.py:Test_flowdb.test_001
+# pytest -v --capture=no tests/test_flowdb.py
+# pytest -v  tests/test_flowdb.py
 ###############################################################
 
 from __future__ import print_function
@@ -10,12 +10,12 @@ import os
 
 from cloudmesh.common.ConfigDict import ConfigDict
 from cloudmesh.common.util import HEADING
-from cloudmesh.flow import Node
+from cloudmesh.flow import WorkFlowDB
 
 import pytest
 
 @pytest.mark.incremental
-class Test_flownode:
+class Test_flowdb:
 
     # noinspection PyPep8Naming
     def tearDown(self):
