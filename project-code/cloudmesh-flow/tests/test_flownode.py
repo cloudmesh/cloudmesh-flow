c047486e09edf065164b1f80c1e5b02695053f51
Rob Ludwig <rwludwig@iu.edu>
2019-04-18 22:54:06 -0400

    node fixes

--- project-code/cloudmesh-flow/tests/test_flownode.py
+++ project-code/cloudmesh-flow/tests/test_flownode.py
@@ -27,6 +27,7 @@ class Test_flownode:
     def test_get_command(self):
         mynode = Node("test2")
         comm_arr = mynode.get_command()
+        mynode.workflow = "test2"
         assert comm_arr[0] == "python"
         assert comm_arr[1] == "test2-flow.py"
         assert comm_arr[2] == "test2"
