96c88d914fbb0bacdcff80b08e197e18ae21ada7
robludwig <robludwig@gmail.com>
2019-05-01 13:58:55 -0400

    switch imports to cloud providers

--- cloudmesh/flow/example/cloudflow-flow.py
+++ cloudmesh/flow/example/cloudflow-flow.py
@@ -1,24 +1,20 @@
-import sys
-import time
+from cloudmesh.compute.aws import Provider as AWSProvider
+from cloudmesh.compute.azure import AzProvider
 from cloudmesh.flow.FlowDecorator import BaseWorkFlow
-from cloudmesh.cloud
 
 class MyFlow(BaseWorkFlow):
-    def spawn_aws_box(self):
-        time.sleep(10)
-        return {"spawned" : true, cloud : "aws", "ip" : "192.28.72.3"}
-
-
-    def spawn_azure_box(self):
-        time.sleep(13)
-        return {"spawned" : true, cloud : "azure", "ip" : "192.28.72.3"}
-
-    def ping_aws_box(self):
-
+    def spawn_aws(self):
         pass
 
-    def ping_azure_box(self):
+    def spawn_azure(self):
         pass
+
+    def ping_aws(self):
+        pass
+
+    def ping_azure(self):
+        pass
+    
 
 if __name__ == "__main__":
     Flow = MyFlow(sys.argv[0])
