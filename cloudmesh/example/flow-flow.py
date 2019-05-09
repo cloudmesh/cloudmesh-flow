88aeface706e8a47db936635168b158a330705ed
Gregor von Laszewski <laszewski@gmail.com>
2019-05-09 06:23:16 -0400

    moving the examples out of the main code

--- cloudmesh/example/flow-flow.py
+++ cloudmesh/example/flow-flow.py
@@ -4,12 +4,15 @@ from cloudmesh.flow.FlowDecorator import BaseWorkFlow
 
 
 class MyFlow(BaseWorkFlow):
+
     def a(self):
         print("in a!")
         time.sleep(5)
+
     def b(self):
         print("in b!")
         time.sleep(10)
+
     def c(self):
         print("in c!")
         time.sleep(10)
